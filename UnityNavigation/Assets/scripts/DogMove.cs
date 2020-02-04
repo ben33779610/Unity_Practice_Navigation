1e68cdf26188a9c02f19e2a08d569a0f48ebb3f6
ben33779610 <49440361+ben33779610@users.noreply.github.com>
2020-02-04 15:35:02 +0800

    隨機移動

---
+++ UnityNavigation/Assets/scripts/DogMove.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class DogMove : MonoBehaviour
+{
+    private NavMeshAgent agent;    //宣告NavMeshAgent
+    private Animator ani;
+
+    public float xMin;
+    public float zMin;
+    public float xMax;
+    public float zMax;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        ani = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move() 
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            {
+            float randX = Random.Range(xMin, xMax);
+            float randZ = Random.Range(zMin, zMax);
+            ani.SetBool("跑步開關",true);
+            agent.destination = new Vector3(randX,transform.position.y ,randZ);
+        }
+    }
+
+}
